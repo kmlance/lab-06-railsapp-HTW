c294ab2a5323bbeb53e7873454ea87dbcede0e6f
Julian Fiebig <julianfiebig@googlemail.com>
2015-01-29 12:07:39 +0100

    Update batman_joker.rb

--- spec/factories/batman_joker.rb
+++ spec/factories/batman_joker.rb
@@ -1,20 +1,14 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1344\cocoasubrtf680
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww12600\viewh13720\viewkind0
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural
-
-\f0\fs22 \cf0 \CocoaLigature0 FactoryGirl.define do\
-  factory :joker, class: Pumpkin do\
-    name \'91Joker\'92\
-    color 'green'\
-    size  \'91l\'92\
-    price \'914.0\'92\
-  end\
-  factory :batman, class: Pumpkin do\
-    name \'91Batman\'92\
-    color \'91blakkk\'92\
-    size  's'\
-    price '5.0'\
-  end\
-end}
+FactoryGirl.define do
+  factory :joker, class: Pumpkin do
+    name  'Joker'
+    color 'green'
+    size  'l'
+    price '4.0'
+  end
+  factory :batman, class: Pumpkin do
+    name  'Batman'
+    color 'blakkk'
+    size  's'
+    price '5.0'
+  end
+end
