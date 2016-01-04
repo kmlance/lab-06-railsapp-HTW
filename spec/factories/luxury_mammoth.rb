1beb272891fc24b9545b9f85e5fe4a5682359824
Daniel Stepanov <danistep@mail.ru>
2016-01-04 17:56:16 +0100

    Create luxury_mammoth.rb

---
+++ spec/factories/luxury_mammoth.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :Winter_Luxury, class: Pumpkin do
+    name 'Winter Luxury'
+    color 'orange'
+    size  's'
+    price '3.3'
+  end
+  factory :Mammoth_Gold, class: Pumpkin do
+    name 'Mammoth Gold'
+    color 'orange'
+    size  'xl'
+    price '55.5'
+  end
+end
