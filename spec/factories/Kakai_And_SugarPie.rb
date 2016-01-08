b6e3413895fb8874beaefdc09934abf0e144afd7
Franziska Henke <franzihenke@hotmail.de>
2016-01-08 12:10:25 +0100

    Added Pumpkins to Repository

---
+++ spec/factories/Kakai_And_SugarPie.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :awesomeKakai, class: Pumpkin do
+    name 'awesomeKakai'
+    color 'sparklyPink'
+    size  'xxxxl'
+    price '7.0'
+  end
+  factory :awesomeSugarPie, class: Pumpkin do
+    name 'awesomeSugarPie'
+    color 'black'
+    size  'xxl'
+    price '25.0'
+  end
+end
