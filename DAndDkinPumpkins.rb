dac48fce1134a77b68e4f0296a8720d687bdde99
FearonLP <fearonlp@web.de>
2016-01-19 15:49:31 +0100

    Create DAndDkinPumpkins.rb
    created some D&D themed pumpkins

---
+++ DAndDkinPumpkins.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+factory :Goblinhead, class: Pumpkin do # by Philipp Wolf s0549932
+  name 'Goblinhead'
+  color 'Snot-green with an bunch of white dots shaped like a grin'
+  size  's'
+  price '16.99'
+end
+ 
+factory :Mimic, class: Pumpkin do # by Philipp Wolf s0549932
+  name 'Mimic'
+  color 'Treasure-chest-brown with a triangular gap, diplaying red flesh up front'
+  size  'XL'
+  price '46.99'
+  end
+end
